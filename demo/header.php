61c8e5478b6fe51c3d3ad6b42ab5e3b7ac8daea2
Soaib Momin <smomin20@gmail.com>
2017-10-08 13:55:42 -0400

    changes header tab links

--- demo/header.php
+++ demo/header.php
@@ -1,45 +1,4 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="robots" content="all,follow">
-    <meta name="googlebot" content="index,follow,snippet,archive">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Universal - All In 1 Template</title>
-
-    <meta name="keywords" content="">
-
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- Bootstrap and Font Awesome css -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-
-    <!-- Css animations  -->
-    <link href="css/animate.css" rel="stylesheet">
-
-    <!-- Theme stylesheet, if possible do not edit this stylesheet -->
-    <link href="css/style.blue.css" rel="stylesheet" id="theme-stylesheet">
-
-    <!-- Custom stylesheet - for your changes -->
-    <link href="css/custom.css" rel="stylesheet">
-
-    <!-- Responsivity for older IE -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-
-    <!-- Favicon and apple touch icons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
-</head>
 <header>
 
             <!-- *** TOP ***
@@ -114,11 +73,11 @@ _________________________________________________________ -->
                                 <li class="dropdown">
                                     <a href="javascript: void(0)" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
                                     <ul class="dropdown-menu">
-                                        <li><a href="contact.html">About Us</a>
+                                        <li><a href="about.php">About Us</a>
                                         </li>
-                                        <li><a href="contact2.html">Contact</a>
+                                        <li><a href="contact.php">Contact</a>
                                         </li>
-                                        <li><a href="contact3.html">FAQ</a>
+                                        <li><a href="faq.php">FAQ</a>
                                         </li>
 
                                     </ul>
