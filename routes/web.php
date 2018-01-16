561f43383e3bb63c70bb0891668e72d3e1e177e0
johneylin <johneylin1314@gmail.com>
2018-01-16 01:07:23 -0500

    newsFeed

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,7 @@ Route::get('careers/link/indicators/{id}', function ($id) {
 
 Route::post('/send','ContactController@sendEmail');
 Route::post('/subscribe','HomeController@joinNewsletter');
+Route::get('/newsfeed','NewsFeedController@Userindex');
 
 
 
@@ -88,6 +89,7 @@ Route::resource('insights','InsightController');
 Route::resource('indicators','IndicatorController');
 Route::resource('sliders','SliderController');
 Route::resource('careers','CareerController');
+Route::resource('newsfeeds','NewsFeedController');
 Route::resource('contact','ContactController');
 Route::resource('admin','AdminController');
 Route::resource('admin/login','AdminLoginController');
